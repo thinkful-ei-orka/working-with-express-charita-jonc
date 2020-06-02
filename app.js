b7bddffa6ae0b129836b96bd102ec1022abe64e9
Jonathan Clark <jonclark822@gmail.com>
2020-06-02 13:29:36 -0700

    added genre filtering

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const express = require("express");
+const express = require('express');
 
-const playstore = require("./playstore.js");
+const playstore = require('./playstore.js');
 
 const app = express();
 
-app.get("/apps", (req, res) => {
-  const { app = "", sort, genere } = req.query;
+app.get('/apps', (req, res) => {
+  const { app = '', sort, genre = '' } = req.query;
 
   if (sort) {
-    if (!["App", "rating"].includes(sort)) {
-      return res.status(400).send("Sort must be one of App or rating");
+    if (!['App', 'rating'].includes(sort)) {
+      return res.status(400).send('Sort must be one of App or rating');
     }
   }
 
-  let results = playstore.filter((apps) =>
-    apps.App.toLowerCase().includes(app.toLowerCase())
-  );
+  let results = playstore;
+
+  if (app) {
+    results = playstore.filter((apps) =>
+      apps.App.toLowerCase().includes(app.toLowerCase())
+    );
+  }
+  if (genre) {
+    results = playstore.filter((apps) =>
+      apps.Genres.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
 
   if (sort) {
     results.sort((a, b) => {
@@ -27,5 +36,5 @@ app.get("/apps", (req, res) => {
 });
 
 app.listen(8000, () => {
-  console.log("Server started on PORT 8000");
+  console.log('Server started on PORT 8000');
 });
